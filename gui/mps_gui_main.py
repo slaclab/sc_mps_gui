a45f9378a91d9b03890cc1556b1da0b04c21e759
Zach Domke <zdomke@slac.stanford.edu>
2022-10-19 16:27:17 -0700

    Adding recent faults tab

--- gui/mps_gui_main.py
+++ gui/mps_gui_main.py
@@ -5,9 +5,11 @@ from models_pkg.mps_model import MPSModel
 from mixins.summary import SummaryMixin
 from mixins.logic import LogicMixin
 from mixins.selection_detail import SelectionDetailsMixin
+from mixins.recent_faults import RecentFaultsMixin
 
 
-class MpsGuiDisplay(Display, SummaryMixin, LogicMixin, SelectionDetailsMixin):
+class MpsGuiDisplay(Display, SummaryMixin, LogicMixin, SelectionDetailsMixin,
+                    RecentFaultsMixin):
     def __init__(self, parent=None, args=[], macros=None):
         super(MpsGuiDisplay, self).__init__(parent=parent, args=args,
                                             macros=macros)
@@ -24,10 +26,12 @@ class MpsGuiDisplay(Display, SummaryMixin, LogicMixin, SelectionDetailsMixin):
         self.logic_init()
         self.selection_init()
         self.summary_init()
+        self.recent_init()
 
         self.logic_slot_connections()
         self.selection_slot_connections()
         self.summ_slot_connections()
+        self.recent_connections()
 
     # ~~~~ PyDM UI File Management ~~~~ #
     @staticmethod
